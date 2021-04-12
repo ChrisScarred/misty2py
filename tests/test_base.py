a4d62296db2cf592325b5cbbc90057a4be189b54
ChrisScarred <scarred.chris@gmail.com>
2021-04-12 20:37:42 +0200

    shortened the script

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,24 +1,20 @@
-from misty2py.robot import Misty
 import json
 from os import path
 import time
 
-this_directory = path.abspath(path.dirname(__file__))
-action_file = str(path.join(this_directory, "custom_allowed_actions.json"))
-data_file = str(path.join(this_directory, "custom_allowed_data.json"))
-info_file = str(path.join(this_directory, "custom_allowed_infos.json"))
+from misty2py.robot import Misty
 
-f = open(info_file)
-custom_allowed_infos = json.loads(f.read())
-f.close()
-f = open(data_file)
-custom_allowed_data = json.loads(f.read())
-f.close()
-f = open(action_file)
-custom_allowed_actions = json.loads(f.read())
-f.close()
 
-misty_robot = Misty(
+DIR_NAME = path.abspath(path.dirname(__file__))
+
+with open(path.join(DIR_NAME, "custom_allowed_actions.json")) as f:
+    custom_allowed_infos = json.loads(f.read())
+with open(path.join(DIR_NAME, "custom_allowed_data.json")) as f:
+    custom_allowed_data = json.loads(f.read())
+with open(path.join(DIR_NAME, "custom_allowed_infos.json")) as f:
+    custom_allowed_actions = json.loads(f.read())
+
+MISTY = Misty(
     "192.168.0.103",
     custom_info=custom_allowed_infos,
     custom_actions=custom_allowed_actions,
@@ -27,68 +23,58 @@ misty_robot = Misty(
 
 
 def test_misty_print():
-    assert str(misty_robot) == "A Misty II robot with IP address 192.168.0.103"
-
+    assert str(MISTY) == "A Misty II robot with IP address 192.168.0.103"
 
 def test_basic_action_dict_method():
     colours = {"red": "200", "green": "20", "blue": "40"}
-    r = misty_robot.perform_action("led", data=colours)
+    r = MISTY.perform_action("led", data=colours)
     assert r["status"] == "Success"
-
 
 def test_basic_action_string_method():
-    r = misty_robot.perform_action("led", data="led_off")
+    r = MISTY.perform_action("led", data="led_off")
     assert r["status"] == "Success"
-
 
 def test_basic_info():
-    r = misty_robot.get_info("blink_settings")
+    r = MISTY.get_info("blink_settings")
     assert r["status"] == "Success"
-
 
 def test_custom_action_dict_method():
     colours = {"FileName": "s_Amazement.wav"}
-    r = misty_robot.perform_action("audio_play", data=colours)
+    r = MISTY.perform_action("audio_play", data=colours)
     assert r["status"] == "Success"
-
 
 def test_custom_action_string_method():
-    r = misty_robot.perform_action("audio_play", data="amazement")
+    r = MISTY.perform_action("audio_play", data="amazement")
     assert r["status"] == "Success"
-
 
 def test_custom_info():
-    r = misty_robot.get_info("hazards_settings")
+    r = MISTY.get_info("hazards_settings")
     assert r["status"] == "Success"
 
-
 def test_query_params():
-    r = misty_robot.get_info(
+    r = MISTY.get_info(
         "audio_file", {"FileName": "s_Amazement.wav", "base64": "false"}
     )
     assert r["content"][0:10] == b"RIFF$\x07\x06\x00WA"
 
-
 def test_event_type_data():
-    misty_robot.event("subscribe", type="BatteryCharge", name="Battery Event")
+    MISTY.event("subscribe", type="BatteryCharge", name="Battery Event")
     time.sleep(0.5)
-    data = misty_robot.event("get_data", name="Battery Event")
-    misty_robot.event("unsubscribe", name="Battery Event")
+    data = MISTY.event("get_data", name="Battery Event")
+    MISTY.event("unsubscribe", name="Battery Event")
     assert data["status"] == "Success"
-
 
 def test_event_type_log():
-    misty_robot.event("subscribe", type="BatteryCharge", name="Battery Event")
+    MISTY.event("subscribe", type="BatteryCharge", name="Battery Event")
     time.sleep(0.5)
-    data = misty_robot.event("get_log", name="Battery Event")
-    misty_robot.event("unsubscribe", name="Battery Event")
+    data = MISTY.event("get_log", name="Battery Event")
+    MISTY.event("unsubscribe", name="Battery Event")
     assert data["status"] == "Success"
 
-
 def test_event_type_unsubscription():
-    misty_robot.event("subscribe", type="BatteryCharge", name="Battery Event")
+    MISTY.event("subscribe", type="BatteryCharge", name="Battery Event")
     time.sleep(0.5)
-    data = misty_robot.event("get_data", name="Battery Event")
-    misty_robot.event("unsubscribe", name="Battery Event")
-    nodata = misty_robot.event("get_data", name="Battery Event")
+    data = MISTY.event("get_data", name="Battery Event")
+    MISTY.event("unsubscribe", name="Battery Event")
+    nodata = MISTY.event("get_data", name="Battery Event")
     assert data["status"] == "Success" and nodata["status"] == "Failed"
