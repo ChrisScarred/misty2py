bbd83b116768af6ec558511573f284eeacbc3a49
ChrisScarred <scarred.chris@gmail.com>
2021-05-04 22:31:27 +0200

    style: black formatted

--- tests/test_base.py
+++ tests/test_base.py
@@ -25,37 +25,43 @@ MISTY = Misty(
 def test_misty_print():
     assert str(MISTY) == "A Misty II robot with IP address 192.168.0.103"
 
+
 def test_basic_action_dict_method():
     colours = {"red": "200", "green": "20", "blue": "40"}
     r = MISTY.perform_action("led", data=colours)
     assert r["status"] == "Success"
 
+
 def test_basic_action_string_method():
     r = MISTY.perform_action("led", data="led_off")
     assert r["status"] == "Success"
 
+
 def test_basic_info():
     r = MISTY.get_info("blink_settings")
     assert r["status"] == "Success"
+
 
 def test_custom_action_dict_method():
     colours = {"FileName": "s_Amazement.wav"}
     r = MISTY.perform_action("audio_play", data=colours)
     assert r["status"] == "Success"
 
+
 def test_custom_action_string_method():
     r = MISTY.perform_action("audio_play", data="amazement")
     assert r["status"] == "Success"
+
 
 def test_custom_info():
     r = MISTY.get_info("hazards_settings")
     assert r["status"] == "Success"
 
+
 def test_query_params():
-    r = MISTY.get_info(
-        "audio_file", {"FileName": "s_Amazement.wav", "base64": "false"}
-    )
+    r = MISTY.get_info("audio_file", {"FileName": "s_Amazement.wav", "base64": "false"})
     assert r["content"][0:10] == b"RIFF$\x07\x06\x00WA"
+
 
 def test_event_type_data():
     MISTY.event("subscribe", type="BatteryCharge", name="Battery Event")
@@ -64,12 +70,14 @@ def test_event_type_data():
     MISTY.event("unsubscribe", name="Battery Event")
     assert data["status"] == "Success"
 
+
 def test_event_type_log():
     MISTY.event("subscribe", type="BatteryCharge", name="Battery Event")
     time.sleep(0.5)
     data = MISTY.event("get_log", name="Battery Event")
     MISTY.event("unsubscribe", name="Battery Event")
     assert data["status"] == "Success"
+
 
 def test_event_type_unsubscription():
     MISTY.event("subscribe", type="BatteryCharge", name="Battery Event")
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,29 +7,43 @@ from misty2py.utils import *
 def test_random_string(length):
     assert len(get_random_string(length)) == length
 
-@pytest.mark.parametrize("red, green, blue, result", [(0,0,0,{"red": 0, "green": 0, "blue": 0}), (150,50,250,{"red": 150, "green": 50, "blue": 250}), (255,255,255,{"red": 255, "green": 255, "blue": 255})])
+
+@pytest.mark.parametrize(
+    "red, green, blue, result",
+    [
+        (0, 0, 0, {"red": 0, "green": 0, "blue": 0}),
+        (150, 50, 250, {"red": 150, "green": 50, "blue": 250}),
+        (255, 255, 255, {"red": 255, "green": 255, "blue": 255}),
+    ],
+)
 def test_rgb_basic(red, green, blue, result):
     assert rgb(red, green, blue) == result
+
 
 def test_rgb_assert_1():
     with pytest.raises(AssertionError):
         rgb(300, 0, 0)
 
+
 def test_rgb_assert_2():
     with pytest.raises(AssertionError):
         rgb(-10, 0, 0)
+
 
 def test_rgb_assert_3():
     with pytest.raises(AssertionError):
         rgb(0, 300, 0)
 
+
 def test_rgb_assert_4():
     with pytest.raises(AssertionError):
         rgb(0, -10, 0)
 
+
 def test_rgb_assert_5():
     with pytest.raises(AssertionError):
         rgb(0, 0, 300)
+
 
 def test_rgb_assert_6():
     with pytest.raises(AssertionError):
